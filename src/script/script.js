a02c1ed5a17c9c3195496046f99827ca338a6114
Arti <tri5oddp@gmail.com>
2021-09-24 14:24:45 +0300

    up

--- src/script/script.js
+++ src/script/script.js
@@ -8,10 +8,12 @@ const popupVideo = document.querySelector(".start-screen__popup-video");
 
 function openPopupVideo() {
     popupVideo.classList.remove("hidden");
+    document.body.style.overflowY = 'hidden'
 };
 
 function closePopupVideo() {
     popupVideo.classList.add("hidden");
+    document.body.style.overflowY = 'initial'
 };
 
 openBtnPopupVideo.addEventListener("click", openPopupVideo);
@@ -66,11 +68,13 @@ recallLink.addEventListener("mouseleave", animationRecallOut);
 
     // Слайдер
 
+const mobileWidth = document.body.offsetWidth
 const swiperAbout = new Swiper('.slider-about__swiper', {
     direction: 'horizontal',
     loop: true,
+    initialSlide: 0,
     slidesPerView: 1.3,
-    spaceBetween: 60,
+    spaceBetween: mobileWidth < 510 ? 10 : 60,
 
     navigation: {
       nextEl: '.slider-about__button-right',
@@ -90,14 +94,14 @@ let numSlide = document.querySelector(".slider-aboutabout__fraction-text");
 swiperAbout.on('activeIndexChange', function () {
     currSlide = document.querySelector(".swiper-slide-next").ariaLabel;
     console.log(currSlide);
-    
+
     numSlide.textContent = currSlide;
 });
 
 function addNumSlide () {
     currSlide = document.querySelector(".swiper-slide-active").ariaLabel;
     console.log(currSlide);
-    
+
     numSlide.textContent = currSlide;
 };
 
@@ -106,25 +110,48 @@ document.addEventListener("DOMContentLoaded", addNumSlide);
 const swiperServices = new Swiper('.slider-services__swiper', {
     direction: 'horizontal',
     loop: false,
-    slidesPerView: 4.2,
-    initialSlide:1
+    slidesPerView: mobileWidth < 510 ? 1.2 : 4.2,
+    initialSlide: mobileWidth < 510 ? 0 : 1,
   });
 
   // Услуги
 
-let servDescrWrapper = document.querySelector(".price__service-description");
-let servDescr = document.querySelector(".service-description__wrapper");
-console.log(servDescrWrapper);
+    const blockService = document.querySelectorAll('.service-description-initialView')
+    const blockServiceMain = document.querySelectorAll('.price-service-main')
 
+    blockService.forEach((r, index) => r.addEventListener('mouseenter', () => {
+        blockServiceMain.forEach((p,i) => {
+            if(i === index){
+                p.classList.toggle('price-service-main-active')
+                r.classList.toggle('service-description-initialView-false')
+            }
+        })
+    } ))
+    blockServiceMain.forEach((v, z) => v.addEventListener('mouseleave', () => {
+        blockService.forEach((k,q) => {
+            if(q === z){
+                k.classList.toggle('service-description-initialView-false')
+                v.classList.toggle('price-service-main-active')
+            }
+        })
+    }))
 
-servDescrWrapper.addEventListener("mouseenter", () => {
-    servDescr.classList.toggle("service-description__wrapper--none");
+//----------------------------------MobileMenu----------------------------------
 
-});
-servDescrWrapper.addEventListener("mouseleave", () => {
-    servDescr.classList.toggle("service-description__wrapper--none");
+    document.addEventListener("DOMContentLoaded", () => {
+        let menu = document.querySelector('.mobile___menu')
+        let navigation = document.querySelector('.header__navigation.navigation')
+        let linkInMenu = document.querySelectorAll('.navigation__link')
+        let activeMenu = document.body
 
-});
+        const clickOnBurger = () => {
+            navigation.classList.toggle('active')
+            menu.classList.toggle('active')
+            activeMenu.classList.toggle('menuActive')
+        }
+        menu.addEventListener('click', clickOnBurger )
+        linkInMenu.forEach(r=> r.addEventListener('click', clickOnBurger ))
 
+    })
 
 
